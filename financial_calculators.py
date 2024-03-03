912f726be318ce40090673c44b5dae551025dd83
Admin <admin@admins-macbook.home>
2024-03-03 16:22:10 +0000

    Script has been brought into line with PEP8 guidelines

--- financial_calculators.py
+++ financial_calculators.py
@@ -1,4 +1,5 @@
-# This is a customer-specific program that allows users to access two interest calculators:
+# This is a customer-specific program that allows users to access two interest
+# calculators:
 #   1. An investment calculator
 #   2. A home loan repayment calculator
 
@@ -11,8 +12,10 @@ Import math functions
 Print line briefly describing investment calculator function
 Print line briefly describing bond calculator function
 
-While True loop with if-elif-else block to determine user choice 'investment' or 'bond'.
-    ask user to choose investment or bond, make lower case and store into the variable 'calc_choice'.
+While True loop with if-elif-else block to determine user choice 'investment'
+or 'bond'.
+    ask user to choose investment or bond, make lower case and store into the 
+    variable 'calc_choice'.
     if 'investment'
         return confirmation
         break
@@ -28,7 +31,8 @@ Request user currency and store into the variable 'currency'.
 Boolean if calc_choice is "investment"
     
     While True loop with try-except block to retrieve user deposit amount
-        Request user deposit amount and store into the string variable 'deposit_amount'.
+        Request user deposit amount and store into the string variable
+        'deposit_amount'.
         try
             cast 'deposit_amount' to float
             return confirmation
@@ -37,7 +41,8 @@ Boolean if calc_choice is "investment"
             return error message
     
     While True loop with try-except block to retrieve user interest rate
-        Request user interest rate and store into the string variable 'interest_rate'.
+        Request user interest rate and store into the string variable
+        'interest_rate'.
         try
             cast 'interest_rate' to float
             return confirmation
@@ -45,8 +50,10 @@ Boolean if calc_choice is "investment"
         except error
             return error message
         
-    While True loop with try-except block to retrieve user investment term length in years
-        Request user term_years and store into the string variable 'term_years'.
+    While True loop with try-except block to retrieve user investment term
+    length in years
+        Request user term_years and store into the string variable
+        'term_years'.
         try
             cast 'term_years' to integer
             return confirmation
@@ -54,8 +61,10 @@ Boolean if calc_choice is "investment"
         except error
             return error message
     
-    While True loop with if-elif-else block to determine user choice 'simple' or 'compound'.
-        ask user to choose simple or compound, make lower case and store into the variable 'interest_type'.
+    While True loop with if-elif-else block to determine user choice 'simple'
+    or 'compound'.
+        ask user to choose simple or compound, make lower case and store into
+        the variable 'interest_type'.
         if 'simple'
             return confirmation
             break
@@ -65,8 +74,10 @@ Boolean if calc_choice is "investment"
         else
             return error message
     
-    While True loop with if-elif-else block to determine user choice 'simple' or 'compound' interest.
-    ask user to choose simple or compoundd, make lower case and store into the variable 'interest_type'.
+    While True loop with if-elif-else block to determine user choice 'simple'
+    or 'compound' interest.
+    ask user to choose simple or compoundd, make lower case and store into the
+    variable 'interest_type'.
     if 'simple'
         break
     elif 'compound'
@@ -79,7 +90,8 @@ Boolean if calc_choice is "investment"
     if "compound"
         interest = deposit_amount * math.pow(1+(interest_rate/100),term_years)
     
-    Subtract deposit_amount from interest and store into the variable 'interest_only'.
+    Subtract deposit_amount from interest and store into the variable
+    'interest_only'.
         
     Return summary of user information
     Return total return with currency
@@ -89,7 +101,8 @@ Boolean if calc_choice is "investment"
 Boolean if calc_choice is "bond"
 
     While True loop with try-except block to retrieve user house value
-        Request user house value and store into the string variable 'house_value'.
+        Request user house value and store into the string variable
+        'house_value'.
         try
             cast 'house_value' to float
             return confirmation
@@ -97,8 +110,9 @@ Boolean if calc_choice is "bond"
         except error
             return error message
 
-   While True loop with try-except block to retrieve user interest rate
-        Request user interest rate and store into the string variable 'interest_rate'.
+    While True loop with try-except block to retrieve user interest rate
+        Request user interest rate and store into the string variable
+        'interest_rate'.
         try
             cast 'interest_rate' to float
             return confirmation
@@ -106,8 +120,10 @@ Boolean if calc_choice is "bond"
         except error
             return error message
 
-    While True loop with try-except block to retrieve user investment term length in months
-        Request user term_months and store into the string variable 'term_months'.
+    While True loop with try-except block to retrieve user investment term
+    length in months
+        Request user term_months and store into the string variable
+        'term_months'.
         try
             cast 'term_months' to integer
             return confirmation
@@ -115,9 +131,11 @@ Boolean if calc_choice is "bond"
         except error
             return error message
             
-    repayment = (interest_rate * house_value) / ( 1 - (1 + interest_rate) ** (- term_months))
+    repayment = (interest_rate * house_value)
+                / ( 1 - (1 + interest_rate) ** (- term_months))
 
-    Subtract house_value from interest and store into the variable 'interest_only'.
+    Subtract house_value from interest and store into the variable
+    'interest_only'.
         
     Return summary of user information
     Return monthly repayments with currency
@@ -131,12 +149,18 @@ Print 'thank you' line.
 import math
 
 # User selection screen for calculator type with explanation
-print("\ninvestment\t- to calculate the amount of interest you'll earn on your investment")
+print(
+    "\ninvestment\t- to calculate the amount of interest you'll earn on your "
+    "investment"
+)
 print("bond\t\t- to calculate the amount you'll have to pay on a home loan")
 
 # While True loop with if-elif-else block to idenify user choice
 while True:
-    calc_choice = input("\nEnter either 'investment' or 'bond' from the menu above to proceed: ").lower()
+    calc_choice = input(
+        "\nEnter either 'investment' or 'bond' from the menu above to "
+        "proceed: "
+    ).lower()
     if calc_choice == "investment":
         print("\nThank you for choosing the Investment Calculator.")
         break
@@ -156,7 +180,9 @@ if calc_choice == "investment":
 
 # While True loop with try-except block to retrieve user deposit amount
     while True:
-        deposit_amount = input(f"Please enter the amount in {currency} you plan to invest:\t")
+        deposit_amount = input(
+            f"Please enter the amount in {currency} you plan to invest:\t"
+        )
         try:
             deposit_amount = float(deposit_amount)
             break
@@ -172,7 +198,7 @@ if calc_choice == "investment":
         except ValueError:
             print("Please enter only numbers.")
 
-# While True loop with try-except block to retrieve user investment term in years
+# Retrieve user investment term in years
     while True:
         term_years = input("Please enter the investment term in years:\t\t")
         try:
@@ -182,33 +208,49 @@ if calc_choice == "investment":
             print("Please enter only whole numbers.")
 
 # User selection screen for interest type with explanation
-    print("\nsimple interest \t- is calculated annually on the initial amount of investment")
-    print("compound interest\t- is calculated annually on the current amount in the investment")
+    print(
+        "\nsimple interest \t- is calculated annually on the initial amount "
+        "of investment"
+    )
+    print(
+        "compound interest\t- is calculated annually on the current amount "
+        "in the investment"
+    )
 
 # While True loop with if-elif-else block to idenify type of interest
     while True:
-        interest_type = input("\nEnter either 'simple' or 'compound' to proceed:\t\t").lower()
-        if interest_type == "simple":
+        interest_type = input(
+            "\nEnter either 'simple' or 'compound' to proceed:\t\t"
+        ).lower()
+        if interest_type in ["simple", "compound"]:
             break
-        elif interest_type == "compound":
-            break
-        else:
-            print("\nI'm sorry, I didn't understand that.")
+        print("\nI'm sorry, I didn't understand that.")
 
-# Check for interest, make appropriate calculation and store into the variable 'interest'. 
+# Ascertain user interest type choice
     if interest_type == "simple":
-        interest = deposit_amount * ( 1 + (interest_rate/100)*term_years)
+        interest = deposit_amount * ( 1 + (interest_rate / 100) * term_years)
     elif interest_type == "compound":
-        interest = deposit_amount * math.pow(1+(interest_rate/100),term_years)
-    
+        interest = deposit_amount * math.pow(1 + (interest_rate / 100),
+                                            term_years)
+
 # Calculate the interest as a separate variable
     interest_only = interest - deposit_amount
 
 # Return the calculated values
     print("\nThank you.")
-    print(f"\nWith {interest_type} interest, your initial investment of {currency}{str(round(deposit_amount,2))} over a {term_years} year term will:" )
-    print(f"\nGive a total return on investment of:\t\t\t{currency}{str(round(interest,2))}")
-    print(f"Including an interest amount of:\t\t\t{currency}{str(round(interest_only,2))}")
+    print(
+        f"\nWith {interest_type} interest, your initial investment of "
+        f"{currency}{str(round(deposit_amount,2))} over a {term_years} year "
+        "term will:" 
+        )
+    print(
+        "\nGive a total return on investment of:\t\t\t"
+        f"{currency}{str(round(interest,2))}"
+    )
+    print(
+        "Including an interest amount of:\t\t\t"
+        f"{currency}{str(round(interest_only,2))}"
+    )
 
 # Bond calculator
 
@@ -217,7 +259,9 @@ if calc_choice == "bond":
 
 # While True loop with try-except block to retrieve user house value
     while True:
-        house_value = input(f"Please enter the present house value in {currency}:\t\t")
+        house_value = input(
+            f"Please enter the present house value in {currency}:\t\t"
+        )
         try:
             house_value = float(house_value)
             break
@@ -233,9 +277,11 @@ if calc_choice == "bond":
         except ValueError:
             print("Please enter only numbers.")
 
-# While True loop with try-except block to retrieve user investment term in months
+# Retrieve user investment term in months
     while True:
-        term_months = input("Please enter the repayment period in months:\t\t")
+        term_months = input(
+            "Please enter the repayment period in months:\t\t"
+        )
         try:
             term_months = int(term_months)
             break
@@ -243,8 +289,11 @@ if calc_choice == "bond":
             print("Please enter only whole numbers.")
 
 # Calculate repayment amount
-    month_interest = (interest_rate/100)/12
-    repayment = (month_interest * house_value) / ( 1 - (1 + month_interest) ** (- term_months))
+    month_interest = (interest_rate / 100) / 12
+    repayment = (
+        (month_interest * house_value)
+        / (1 - (1 + month_interest) ** (- term_months))
+    )
 
 # Calculate total cost
     total_bond = repayment * term_months
@@ -254,10 +303,23 @@ if calc_choice == "bond":
 
 # Return the calculated values
     print("\nThank you.")
-    print(f"\nFor a bond value of {currency}{str(round(house_value,2))}, repaid over {term_months} months at an interest rate of {interest_rate}%: ")
-    print(f"\nYour monthly repayments will be \t\t\t{currency}{str(round(repayment,2))}")
-    print(f"This bond will cost you a total of:\t\t\t{currency}{str(round(total_bond,2))}")
-    print(f"Including an interest amount of:\t\t\t{currency}{str(round(interest_only,2))}")
+    print(
+        f"\nFor a bond value of {currency}{str(round(house_value,2))}, "
+        "repaid over {term_months} months at an interest rate of "
+        f"{interest_rate}%: "
+    )
+    print(
+        "\nYour monthly repayments will be \t\t\t"
+        f"{currency}{str(round(repayment,2))}"
+    )
+    print(
+        "This bond will cost you a total of:\t\t\t"
+        f"{currency}{str(round(total_bond,2))}"
+    )
+    print(
+        "Including an interest amount of:\t\t\t"
+        f"{currency}{str(round(interest_only,2))}"
+        )
 
 # Line of text to thank the user
 print("\nThank you for using this calculator. Have a nice day.")
